b8cac644c702462a11f4db516baf55df8b207055
jackim95 <48398924+jackim95@users.noreply.github.com>
2023-11-01 09:35:03 -0600

    Update taglookupdate.py

--- PointMapping/taglookupdate.py
+++ PointMapping/taglookupdate.py
@@ -1,6 +1,11 @@
 import pickle
+import brickschema as br
 
-with open(r"C:\Users\ikim2\AppData\Local\anaconda3\envs\afdd_library\Lib\site-packages\brickschema\ontologies\1.3\taglookup_origin.pickle", 'rb') as f:
+
+path = br.__file__.replace(r'\__init__.py', r'\ontologies\1.3\taglookup.pickle')
+
+
+with open(path, 'rb') as f:
     data = pickle.load(f)
 
 data[('Supply', 'Fan', 'Speed')] = {'Supply_Fan_Speed'}
@@ -9,6 +14,5 @@ data[('Chiller', 'Power')] = {'Chiller_Power'}
 data[('Date', 'Time')] = {'Date_Time'}
 
 
-with open(r"C:\Users\ikim2\AppData\Local\anaconda3\envs\afdd_library\Lib\site-packages\brickschema\ontologies\1.3\taglookup.pickle", 'wb') as d:
+with open(path, 'wb') as d:
     pickle.dump(data, d)
-
